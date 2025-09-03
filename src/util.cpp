4c3fab828685698bee370c97c6121fc63c41a578
mmjwxbc <2891002167@qq.com>
2025-09-03 22:29:40 +0800

    Download a piece through magnet

--- src/util.cpp
+++ src/util.cpp
@@ -272,7 +272,8 @@ int handle_magnet_handshake(const string ip, const uint16_t port, const string h
     recv_buf.erase(recv_buf.begin(), recv_buf.begin() + prefix_len - 2);
     cout << "Peer Metadata Extension ID: " << extension_object["m"]["ut_metadata"] << endl;
 
-    metadata_id = extension_object["m"]["ut_metadata"].get<unsigned int>();
+    metadata_id = extension_object["m"]["ut_metadata"].get<uint8_t>();
+    cout << "handle_magnet_handshake = " << metadata_id << endl;
 
 
     // // send interest message
